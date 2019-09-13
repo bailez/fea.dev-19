2c1d798b7ba40bf4f150dc96703ba1dcaa1f6d4e
bailez <bailez@usp.br>
2019-09-12 22:26:07 -0300

    Explicaçao sobre importaçoes

--- modulos/exemplo.py
+++ modulos/exemplo.py
@@ -6,3 +6,7 @@ Este módulo possue uma função que escreve uma mensagem
 
 def mensagem():
     print("Você importou este módulo!")
+
+
+def soma(x,y):
+    return x+ y
--- modulos/principal.py
+++ modulos/principal.py
@@ -1,5 +1,36 @@
-""" Para termos as funções e métodos de outro módulo devemos importar o módulo e desta forma conseguimos executar 
+""" 
+Para termos as funções e métodos de outro módulo devemos importar o módulo e desta forma conseguimos executar 
 """
 import exemplo
 
 exemplo.mensagem()
+
+"""
+É possível também apenas importar uma função específica do modulo usando a seguinte sintaxe,
+ seguindo esta lógica é possível executar a função de um módulo diretamente sem precisar chamar o módulo antes ao longo do script
+"""
+
+from exemplo import mensagem
+
+mensagem()
+
+"""
+Para utilizar o módulo ou função com outro nome podemos fazer da sguinte forma:
+"""
+
+import exemplo as ex
+
+ex.mensagem()
+
+from exemplo import mensagem as msg
+
+msg()
+
+""" 
+Podemos também importar todos as funções presentes dentro de um módulo usando '*':
+"""
+
+from exemplo import *
+
+mensagem()
+print(soma(4,5))
