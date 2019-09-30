9c0d0ad6d4a05ec51fc9578b3fa6231ab660bc38
bailez <bailez@usp.br>
2019-09-30 17:50:05 -0300

    ajuste gerenciador

--- exercicios/Classes e Modulos/gerenciador.py
+++ exercicios/Classes e Modulos/gerenciador.py
@@ -12,23 +12,28 @@ class Gerenciador:
         if perfil == 'm':
             if risco > 25 and risco <75:
                 print(f'O ativo {ativo.nome} é recomendado para {pessoa.nome}')
-                pessoa.comprar(ativo,1)
+                #pessoa.comprar(ativo,1)
+                return True
             else:
                 print(f'O ativo {ativo.nome} não é recomendado para {pessoa.nome}')
+                return False
         # r para arriscador
         elif perfil == 'r':
             if risco <75:
                 print(f'O ativo {ativo.nome} é recomendado para {pessoa.nome}')
-                pessoa.comprar(ativo,1)
+                #pessoa.comprar(ativo,1)
+                return True
             else:
                 print(f'O ativo {ativo.nome} não é recomendado para {pessoa.nome}')
-            
+                return False
         # a para avesso o risco
         elif perfil == 'a':
             if risco > 25:
                 print(f'O ativo {ativo.nome} é recomendado para {pessoa.nome}')
-                pessoa.comprar(ativo,1)
+                #pessoa.comprar(ativo,1)
+                return True
             else:
                 print(f'O ativo {ativo.nome} não é recomendado para {pessoa.nome}')
+                return False
         else:
             print(f'{pessoa.nome} não possui perfil valido')
